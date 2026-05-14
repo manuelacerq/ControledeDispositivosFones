758b00e7f4f5a1a22cab3bb60cc80a44cbad30bc
manuelacerq <132608806+manuelacerq@users.noreply.github.com>
2026-05-14 10:04:56 -0300

    Update BuscaFones.py

--- BuscaFones.py
+++ BuscaFones.py
@@ -6,15 +6,19 @@ URL = "https://docs.google.com/spreadsheets/d/1-5bvXP3GrPCCX52XOPKSsy0pL_0WTR5CO
 def consultar_dispositivo(nome_dispositivo):
     df = pd.read_csv(URL)
 
-    st.write(df.columns)
+    col_dispositivo = "Qual fone de ouvido você está tomando posse?"
+    col_nome = "Qual o seu nome completo?"
+    col_local = "Em qual localidade o dispositivo estará sendo utilizado? (ex: EAJ, LAIS, etc)"
 
-    df.columns = df.columns.str.strip()
+    df_filtrado = df[df[col_dispositivo] == nome_dispositivo]
+
+    if df_filtrado.empty:
+        return None
+
+    ultimo = df_filtrado.iloc[-1]
+    return ultimo[col_nome], ultimo[col_local]
+
 st.title("🎧 Controle de Fones")
-
-dispositivo = st.selectbox(
-    "Selecione o dispositivo:",
-    ["Fone 1", "Fone 2", "Fone 3", "Fone 4", "Fone 5", "Fone 6", "Fone 7", "Fone 8", "Fone 9", "Fone 10"]
-)
 
 if st.button("Consultar"):
     resultado = consultar_dispositivo(dispositivo)
